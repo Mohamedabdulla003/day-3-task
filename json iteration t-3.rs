d0043161da2f98b22f2d53325a8d2d84b8189808
Mohamedabdulla003 <149222807+Mohamedabdulla003@users.noreply.github.com>
2023-11-20 12:20:29 +0530

    Update json iteration t-3.rs

--- json iteration t-3.rs
+++ json iteration t-3.rs
@@ -7,3 +7,27 @@ let obj1 = {
     name ;"abdulla"
  }
  
+ let keys1 = object.keys(obj1)
+ let keys2 = object.keys(obj2)
+ console.log(keys1,keys2)
+
+ let flag = true;
+  
+ if(keys1.length == keys2.length){
+   for(ley key in obj){
+ 
+      if(obj1[key] != obj2[key]){
+         flag = true
+      }
+
+
+   } 
+}else{
+   flag=false
+}
+
+if(flage==true){
+   console.log{"object are same"}
+else{
+   console.log("object are not same")
+}   
